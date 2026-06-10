6475d424b1f52f541d32475b8ab29eca0a40e6a0
ruben menalled <rubenmenalled@rubens-MacBook-Air.local>
2026-06-09 22:27:28 -0300

    Botones AGREGAR: carrito de vuelta, centrado (sin espacio extra)

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -171,7 +171,7 @@ function BuscarContent() {
                             minOrder: p.minOrder,
                           })
                         }}>
-                        AGREGAR
+                        <ShoppingCart size={12} />AGREGAR
                       </motion.button>
                     </div>
                   </motion.div>
